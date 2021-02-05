def9899a4b071c74076e50f693a0f502c940efb3
Mrs Sharmin Akter <zamalzamalhossain@gmail.com>
2021-02-05 12:11:37 +0100

    added file

---
+++ window.js
@@ -0,0 +1,13 @@
+var name = 'Sohag';
+function add(num1, num2){
+    var result = num1 + num2;
+    console.log('name inside', name);
+    function double(num){
+        return num * 2;
+    }
+    var total = double(result)
+    return total;
+}
+console.log('name outside', name);
+var sum = add(2, 43);
+console.log(sum);
